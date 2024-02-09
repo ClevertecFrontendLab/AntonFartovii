8aae647d3b26cb6ed816685e4a34ef067f2b0b64
AntonFartovii <anton@rza.by>
2024-02-09 11:29:09 +0300

    feat: add sider 1440

--- src/layout/Sider.tsx
+++ src/layout/Sider.tsx
@@ -1,14 +1,49 @@
+import logo from '../assets/svg/logo.svg';
+import exit_icon from '../assets/svg/exit_icon.svg';
+import classes from './layout.module.css';
+import {
+    CalendarOutlined,
+    HeartOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    ProfileOutlined,
+    TrophyOutlined,
+} from '@ant-design/icons';
+
 const Sider = () => {
     return (
-        <div className="sider">
-            <div className="logo">
-                logo
+        <div className={classes.sider}>
+            <div className={classes.logo}>
+                <img src={logo}/>
             </div>
-            <div className="menu">
-                menu
+            <div className={classes.menu}>
+                <div className={classes["menu-link"]}>
+                    <div className={classes["link-icon"]}><CalendarOutlined/></div>
+                    <div className={classes["link-title"]}>Календарь</div>
+                </div>
+
+                <div className={classes["menu-link"]}>
+                    <div className={classes["link-icon"]}><HeartOutlined/></div>
+                    <div className={classes["link-title"]}>Тренировки</div>
+                </div>
+
+                <div className={classes["menu-link"]}>
+                    <div className={classes["link-icon"]}><TrophyOutlined/></div>
+                    <div className={classes["link-title"]}>Достижения</div>
+                </div>
+
+                <div className={classes["menu-link"]}>
+                    <div className={classes["link-icon"]}><ProfileOutlined/></div>
+                    <div className={classes["link-title"]}>Профиль</div>
+                </div>
             </div>
-            <div className="logout">
-                logout
+            <div className={classes.logout}>
+                <div className={classes["logout-icon"]}><img src={exit_icon}/></div>
+                <div className={classes["logout-title"]}>Выйти</div>
+            </div>
+            <div className={classes.switcher}>
+                <MenuUnfoldOutlined className={classes["switcher-icon"]}/>
+                <MenuFoldOutlined hidden/>
             </div>
         </div>
     );
